779721478ecbe1ffcd34d939657fd226e733067a
Fernando Jorge Mota <contato@fjorgemota.com>
2022-06-06 14:49:19 -0300

    Use Handlebars to render template strings
    And add support for NOT having an artifact name to use

--- index.js
+++ index.js
@@ -1,100 +1,120 @@
 const core = require( '@actions/core' );
 const github = require( '@actions/github' );
+const handlebars = require( 'handlebars' );
 
 
 async function run() {
-		const githubToken = core.getInput( 'github-token' );
-		const artifactName = core.getInput('artifact-name');
-		const reportOn = core.getInput('report-on');
-		const commitStatusContext = core.getInput('context');
-		const message = core.getInput('message');
-		const octokit = github.getOctokit( githubToken );
+	const githubToken = core.getInput('github-token');
+	const artifactName = core.getInput('artifact-name') || null;
+	const reportOn = core.getInput('report-on');
+	const commitStatusContext = core.getInput('context');
+	const message = core.getInput('message');
+	const commitStatusText = core.getInput('commit_status_state');
+	const octokit = github.getOctokit(githubToken);
 
-		const { context } = github;
+	const {context} = github;
 
-		const { owner, repo } = context.repo;
+	const {owner, repo} = context.repo;
 
-		if (!context.payload.workflow_run) {
-			throw new Error("This action must run on workflow that runs on: workflow_run, so it can get the artifact list properly");
+	if (!context.payload.workflow_run) {
+		throw new Error("This action must run on workflow that runs on: workflow_run, so it can get the artifact list properly");
+	}
+
+	const {id: run_id, check_suite_id, head_sha: sha, pull_requests} = context.payload.workflow_run;
+
+	const artifactsResponse = await octokit.rest.actions.listWorkflowRunArtifacts(
+		{
+			owner,
+			repo,
+			run_id,
 		}
+	);
 
-		const { id: run_id, check_suite_id, head_sha: sha, pull_requests } = context.payload.workflow_run;
+	const data = formatArtifacts({owner, repo, check_suite_id, sha}, artifactsResponse, artifactName);
 
-		const artifactsResponse = await octokit.rest.actions.listWorkflowRunArtifacts(
-			{
+	switch (reportOn) {
+		case "commit_status":
+			await octokit.rest.repos.createCommitStatus({
 				owner,
 				repo,
-				run_id,
+				sha,
+				state: formatMessage(commitStatusText, data),
+				target_url: data.url,
+				description: formatMessage(message, data),
+				context: formatMessage(commitStatusContext, data),
+			});
+			break;
+		case "pull_request":
+			if (!pull_requests.length) {
+				throw new Error("No pull requests associated with the workflow_run");
 			}
-		);
+			await octokit.rest.issues.createComment({
+				owner,
+				repo,
+				issue_number: pull_requests[0].number,
+				body: formatMessage(message, data),
+			});
+			break;
+		case "none":
+			break;
+	}
+	core.setOutput( 'artifact_id', data.artifact ? data.artifact.id : "" );
+	core.setOutput( 'artifact_url', data.artifact ? data.artifact.url : "" );
+	core.setOutput('artifact_list', data.list)
+}
 
+function formatArtifacts(response, context, artifactName) {
+	const list = response.data.artifacts.map(formatArtifact, context);
+	const artifact = findArtifact(list, artifactName);
+	return {
+		artifact,
+		queriedArtifactName: artifactName,
+		list
+	}
+}
 
-		const artifactResult = artifactsResponse.data.artifacts.find(
-			( artifact ) => {
-				return artifact.name === artifactName;
-			}
-		);
+function formatArtifact(artifactResult) {
+	const { owner, repo, check_suite_id, sha} = this;
+	const artifactId = artifactResult.id;
+	const artifactName = artifactResult.name;
+	const artifactUrl = `https://github.com/${owner}/${repo}/suites/${check_suite_id}/artifacts/${artifactId}`;
 
-
-		const artifactFound = !!artifactResult;
-
-		const artifactId = artifactFound ? artifactResult.id : "ID not found";
-		const artifactUrl = `https://github.com/${owner}/${repo}/suites/${check_suite_id}/artifacts/${artifactId}`;
-
-
-		const artifactData = {
-			id: artifactId,
-			name: artifactName,
-			url: artifactUrl,
-			commit: sha
-		}
-
-		switch ( reportOn ) {
-			case "commit_status":
-				await octokit.rest.repos.createCommitStatus( {
-					owner,
-					repo,
-					sha,
-					state: artifactFound ? 'success' : 'failure',
-					target_url: artifactUrl,
-					description: formatMessage(artifactFound ? message : 'No artifact found with the name "{name}"', artifactData),
-					context: formatMessage(commitStatusContext, artifactData),
-				} );
-				break;
-			case "pull_request":
-				if (!pull_requests.length) {
-					throw new Error("No pull requests associated with the workflow_run");
-				}
-				await octokit.rest.issues.createComment({
-					owner,
-					repo,
-					issue_number: pull_requests[0].number,
-					body: formatMessage(message, artifactData),
-				});
-				break;
-			case "none":
-				break;
-		}
-
-		if (artifactFound) {
-			core.setOutput( 'artifact_id', artifactId );
-			core.setOutput( 'artifact_url', artifactUrl );
-		} else {
-			throw new Error(`No artifact found with the name "${artifactName}"`);
-		}
+	return {
+		id: artifactId,
+		name: artifactName,
+		url: artifactUrl,
+		commit: sha
+	}
 }
 
 function formatMessage(message, obj) {
-	const keys = Object.keys(obj);
-	const regex = new RegExp("{("+keys.join("|")+")}", "g");
-	return message.replace(regex, function(match, key) {
-		return obj[key];
-	})
+	const template = handlebars.compile(message, {strict: true});
+	return template(obj);
 }
 
 function reportErrors(error) {
 	core.info(error);
 	core.setFailed(error.message);
+}
+
+function findArtifact(artifacts, artifactName) {
+	if (!artifactName) {
+		return;
+	}
+
+	const artifactResult = artifacts.find(
+		( artifact ) => {
+			return artifact.name === artifactName;
+		}
+	);
+
+	if (artifactResult === false) {
+		throw new Error(`Artifact '${artifactName} not found`)
+	}
+
+	return {
+		...artifactResult,
+	};
 }
 
 
